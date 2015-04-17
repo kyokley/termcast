aa08d7909a93fbc6a9ca50af44ae8e5122531401
Kevin Yokley <yokley@dev007>
2015-04-16 19:14:03 -0500

    Generating termcast through fabfile

--- fabfile.py
+++ fabfile.py
@@ -3,6 +3,8 @@ from fabric import api as fab
 from fabric import decorators
 from fabric.contrib import files
 
+from termcastTemplates import termcastTemplate, termcastPlayTemplate
+
 import os, getpass
 
 fab.env.colors = True
@@ -16,8 +18,12 @@ telnet stream tcp4 nowait {user} /usr/sbin/tcpd /usr/bin/termcast-play
 telnet stream tcp6 nowait {user} /usr/sbin/tcpd /usr/bin/termcast-play
 '''
 
+
+def write_file(filename, text, use_sudo=False):
+    files.append(filename, text, use_sudo=use_sudo)
+
 def write_sudo_file(filename, text):
-    files.append(filename, text, use_sudo=True)
+    write_file(filename, text, use_sudo=True)
 
 @fab.task
 @decorators.hosts(['localhost'])
@@ -28,9 +34,26 @@ def install():
     for command in COMMANDS:
         fab.local(command)
 
-    values = {'user': user}
+    casts = fab.prompt(colors.cyan('Specify directory where you want the '
+                                      'casts to be stored'),
+                           default=os.path.join(installDir, 'casts'))
 
-    linkCommand = 'sudo ln -s {0} /usr/bin'
+    values = {'user': user,
+              'installDir': installDir,
+              'termcastPath': os.path.join(installDir, 'termcast'),
+              'termcastPlayPath': os.path.join(installDir, 'termcast-play'),
+              'casts': casts}
+
+    try:
+        fab.local('rm {}'.format(values['termcastPath']))
+        fab.local('rm {}'.format(values['termcastPlayPath']))
+    except:
+        pass
+
+    write_file(values['termcastPath'], termcastTemplate.format(**values))
+    write_file(values['termcastPlayPath'], termcastPlayTemplate.format(**values))
+
+    linkCommand = 'sudo ln -s {} /usr/bin'
     for script in SCRIPTS:
         try:
             fab.local(linkCommand.format(os.path.join(installDir, script)))
@@ -41,3 +64,5 @@ def install():
 
     write_sudo_file('/etc/inetd.conf', inetdConfText)
     fab.local('sudo /etc/init.d/openbsd-inetd restart')
+    fab.local('chmod a+x {}'.format(values['termcastPath']))
+    fab.local('chmod a+x {}'.format(values['termcastPlayPath']))
